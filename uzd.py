948d4e2febc83360853c90b511fbaf0f27c06f5f
lianausane <102228831+lianausane@users.noreply.github.com>
2022-05-10 18:52:15 +0300

    turpinat

--- uzd.py
+++ uzd.py
@@ -15,11 +15,32 @@ f = open(fails, 'a', encoding="UTF-8")
 print("Sveiki parbaudīsim vai vari aprēķināt dažādu figūru laukumu.")
 print("Kādā apjomā veiksim darbu?")
 
+p = 0
+n = 0
 apj = 0
 while apjoms < 5 or apjoms > 50:
     if apjoms < 5 or apjoms > 50:
         print("Ievadīts nepareiz apjoms! Ievadi vēlreiz.")
     apjoms = int(input("Ievadi apjomu no 5 līdz 50: ")) 
 
-print("Kādu figūru laukumu aprēķināsi?")
+print("Kādas figūras laukumu aprēķināsi?")
+print("Ja taisnustūra tad ievadi 1")
+print("Ja tristūra tad ievadi 2 ")
+print("Ja apļa tad ievadi 3")
+kf = int(input("kuru figūras laukumu apreķināsim: "))
+uzd = int(input("Cik tādus uzdevumus pildi: "))
+if kf = 1:
+    for i in range(uzd):
+    a = random.randrange(1, apjoms)
+    b = random.randrange(1, apjoms)
+    print(f"Kāds ir taisnustūra laukums ja tā malu garums ir šāds: {a} , {b}")
+    atb = int(input("Laukums ir: "))
+    if atb == a * b:
+        print("Parezi!")
+        p+=1
+    else:
+        print("Neparezi!")
+        n+=1
+    
+    
 
